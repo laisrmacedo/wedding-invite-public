3f781d53e6d567b8e14f880c111c66f237c0572e
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-12-12 20:22:53 -0300

    fix: database

--- invite/src/App.js
+++ invite/src/App.js
@@ -1,8 +1,8 @@
 import { Router } from "./router/Router";
 
 //backend
-// export const BASE_URL = 'http://localhost:3003/'
-export const BASE_URL = 'https://wedding-invite-mock.onrender.com/'
+export const BASE_URL = 'http://localhost:3003/'
+// export const BASE_URL = 'https://wedding-invite-mock.onrender.com/'
 
 function App() {
   return (      
