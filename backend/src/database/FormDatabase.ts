220cf403848981a84a5f388f688f2b3b6468469e
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-12-12 16:05:54 -0300

    fix: base url

--- backend/src/database/FormDatabase.ts
+++ backend/src/database/FormDatabase.ts
@@ -3,47 +3,63 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 //SQLite3
-import * as fs from 'fs';
-import * as path from 'path';
+// import * as fs from 'fs';
+// import * as path from 'path';
 import knex from 'knex';
 
-export class BaseDatabase {
+export abstract class BaseDatabase {
   protected static connection = knex({
-    client: 'pg', // Usar o cliente do PostgreSQL
-    connection: {
-      host: process.env.DB_HOST, // Host do banco de dados
-      port: Number(process.env.DB_PORT), // Porta do banco de dados
-      user: process.env.DB_USER, // Usuário do banco de dados
-      password: process.env.DB_PASSWORD, // Senha do banco de dados
-      database: process.env.DB_NAME, // Nome do banco de dados
-    },
-    pool: {
-      min: 0,
-      max: 1,
-      afterCreate: (conn: any, cb: any) => {
-        conn.query('SET timezone="UTC";', (err: any) => {
-          cb(err, conn);
-        });
+      client: "sqlite3",
+      connection: {
+          filename: process.env.DB_FILE_PATH as string,
       },
-    },
-  });
-
-  // Função para criar a tabela a partir do arquivo 'form.db'
-  public async createFormTable(): Promise<void> {
-    try {
-      const tableExists = await BaseDatabase.connection.schema.hasTable('form');
-      
-      if (!tableExists) {
-        const filePath = path.join(__dirname, 'form.sql'); // Caminho completo para o arquivo
-        const sql = fs.readFileSync(filePath, 'utf-8'); // Ler o conteúdo do arquivo
-        await BaseDatabase.connection.raw(sql); // Executar o SQL para criar a tabela
-        console.log('Tabela "form" criada com sucesso.');
+      useNullAsDefault: true,
+      pool: { 
+          min: 0,
+          max: 1,
+          afterCreate: (conn: any, cb: any) => {
+              conn.run("PRAGMA foreign_keys = ON", cb)
+          }
       }
-    } catch (error) {
-      console.error('Erro ao criar/verificar a tabela "form":', error);
-    }
-  }
+  })
 }
+// export class BaseDatabase {
+//   protected static connection = knex({
+//     client: 'pg', // Usar o cliente do PostgreSQL
+//     connection: {
+//       host: process.env.DB_HOST, // Host do banco de dados
+//       port: Number(process.env.DB_PORT), // Porta do banco de dados
+//       user: process.env.DB_USER, // Usuário do banco de dados
+//       password: process.env.DB_PASSWORD, // Senha do banco de dados
+//       database: process.env.DB_NAME, // Nome do banco de dados
+//     },
+//     pool: {
+//       min: 0,
+//       max: 1,
+//       afterCreate: (conn: any, cb: any) => {
+//         conn.query('SET timezone="UTC";', (err: any) => {
+//           cb(err, conn);
+//         });
+//       },
+//     },
+//   });
+
+//   // Função para criar a tabela a partir do arquivo 'form.db'
+//   public async createFormTable(): Promise<void> {
+//     try {
+//       const tableExists = await BaseDatabase.connection.schema.hasTable('form');
+      
+//       if (!tableExists) {
+//         const filePath = path.join(__dirname, 'form.sql'); // Caminho completo para o arquivo
+//         const sql = fs.readFileSync(filePath, 'utf-8'); // Ler o conteúdo do arquivo
+//         await BaseDatabase.connection.raw(sql); // Executar o SQL para criar a tabela
+//         console.log('Tabela "form" criada com sucesso.');
+//       }
+//     } catch (error) {
+//       console.error('Erro ao criar/verificar a tabela "form":', error);
+//     }
+//   }
+// }
 
 
 
@@ -62,7 +78,7 @@ export class FormDatabase extends BaseDatabase{
 
   //methods
   public async getGuests(){
-    await this.createFormTable()
+    // await this.createFormTable()
 
     const guestDB = await BaseDatabase
       .connection(FormDatabase.TABLE_GUESTS)
